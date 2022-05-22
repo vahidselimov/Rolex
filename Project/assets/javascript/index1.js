a2cb7a8d0890d18173ea0e3a0edf552f9926eef7
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-05-22 21:47:56 +0400

    Rolex part 2 'localstorage'

---
+++ Project/assets/javascript/index1.js
@@ -0,0 +1,39 @@
+let allbtn=document.querySelectorAll(".btn-floating")
+let card_content=document.querySelectorAll(".card-content")
+let price=document.querySelectorAll(".price")
+if (localStorage.getItem("basket")==null) {
+   localStorage.setItem("basket",JSON.stringify([])) 
+}
+// basket=[];
+allbtn.forEach(btn=>{
+    btn.addEventListener("click",function(e){
+        e.preventDefault()
+        let Id=(btn.parentElement.parentElement.getAttribute("data-id"));
+        let image=(this.previousElementSibling.previousElementSibling.getAttribute("src"));
+    let price=(btn.parentElement.parentElement.lastElementChild.lastElementChild.innerText)
+  let title=(btn.parentElement.parentElement.lastElementChild.lastElementChild.previousElementSibling.previousElementSibling.innerText)
+    
+    let marka=(btn.previousElementSibling.innerText)
+        let basket=JSON.parse(localStorage.getItem("basket"))
+        let exisPro=basket.find(p=>p.id==Id)
+        if (exisPro===undefined) {
+            basket.push({
+                id:Id,
+                count:1,
+                image,
+                price,
+                title,
+                marka,
+            })
+        }
+        else{
+exisPro.count++;
+        }
+        localStorage.setItem("basket",JSON.stringify(basket))
+
+       
+        console.log(basket);
+    })
+}
+
+)
